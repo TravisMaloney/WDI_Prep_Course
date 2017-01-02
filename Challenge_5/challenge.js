be0896cba364d961de7bb8f38f67705dd57ca2cb
TravisMaloney <travis.maloney1@gmail.com>
2017-01-02 13:14:51 -0500

    chal5 javascript

---
+++ Challenge_5/challenge.js
@@ -0,0 +1,30 @@
+//Exercise 1
+var name = "travis";
+console.log("Hello " + name);
+
+//Exercise 2
+var x = 1125;
+var answer = x % 2;
+console.log(answer);
+
+//Exercise 3
+var a = true;
+var b = false;
+var c = true;
+console.log(a || b || c);
+
+//Exercise 4
+console.log(a && b && c);
+
+//Exercise 5
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+var answer2 = 10 - (3 *(5/4) + (202 % 3));
+console.log(answer1);
+console.log(answer2);
+
+//Exercise 6
+String(12);
+parseInt("113");
+parseFloat("1.12356");
+var x = parseInt("Hello World");
+console.log(x);
